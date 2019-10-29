b88fd356cf30fe6a59acaccbfa05bd69e392a656
gouyz <413977349@qq.com>
2019-10-30 00:22:58 +0800

    111

---
+++ JSLogistics/Classes/Mine/Controllers/JSLCartOrderDetailVC.swift
@@ -0,0 +1,77 @@
+//
+//  JSLCartOrderDetailVC.swift
+//  JSLogistics
+//  购物订单详情
+//  Created by gouyz on 2019/10/29.
+//  Copyright © 2019 gouyz. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+private let cartOrderDetailCell = "cartOrderDetailCell"
+
+class JSLCartOrderDetailVC: GYZBaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(0)
+        }
+    }
+    lazy var tableView : UITableView = {
+        let table = UITableView(frame: CGRect.zero, style: .grouped)
+        table.dataSource = self
+        table.delegate = self
+        table.separatorStyle = .none
+        table.backgroundColor = kBackgroundColor
+        
+        
+        table.register(JSLCartOrderListCell.classForCoder(), forCellReuseIdentifier: cartOrderListCell)
+        
+        
+        return table
+    }()
+
+}
+extension JSLCartOrderDetailVC: UITableViewDelegate,UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 12
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cartOrderListCell) as! JSLCartOrderListCell
+        
+        cell.selectionStyle = .none
+        return cell
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        return UIView()
+    }
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        
+        return UIView()
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //        goDetailVC()
+    }
+    ///MARK : UITableViewDelegate
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 190
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.00001
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        
+        return 0.00001
+    }
+}
