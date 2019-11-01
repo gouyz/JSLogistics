2df1d1d84a7036fd6bd75708498a38f43af3f38c
gouyz <413977349@qq.com>
2019-11-01 17:24:55 +0800

    1111

---
+++ JSLogistics/Classes/Mine/Controllers/JSLConmentDetailVC.swift
@@ -0,0 +1,30 @@
+//
+//  JSLConmentDetailVC.swift
+//  JSLogistics
+//  查看评价
+//  Created by gouyz on 2019/11/1.
+//  Copyright © 2019 gouyz. All rights reserved.
+//
+
+import UIKit
+
+class JSLConmentDetailVC: GYZBaseVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
